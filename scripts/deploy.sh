efd2cfbd6cfedcddc2f1fbb5b36d23112dff55b1
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-03-10 01:11:18 +0100

    ci(scripts): add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+ 
+#!/bin/bash
+
+# exit with nonzero exit code if anything fails
+set -e
+
+# run the prod build
+npm run build:prod
+
+# deploy with semantic-release
+npm run semantic-release
+
+# just to be sure we exit cleanly
+exit 0;
